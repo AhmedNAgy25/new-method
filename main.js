9c681539976e21605fa7c5099b985478750c4e0d
Ahmed NAgy25 <ahmednagy25t@gmail.com>
2024-07-04 15:38:26 +0200

    go

---
+++ main.js
@@ -0,0 +1,10 @@
+var x=-1;
+
+function change(){
+    x=x*-1;
+    if(x==1){
+        document.getElementById("h").style=" background-color:red";
+    }else{
+        document.getElementById("h").style=" background-color:rgb(55, 176, 212)";
+    }
+}
